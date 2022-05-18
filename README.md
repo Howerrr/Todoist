f2dbc2c912c85d5c535c241582a98dca915aef7c
hower454516977 <454516977@qq.com>
2022-05-18 21:37:34 +0800

    1.1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Retodo
-todoist by React
+## Project Infor
+
+A todo-list web, using react and json-server.
+
+Author: MichaelYuhe
+
+## How to Start
+
+### Front-end
+
+`npm run start`
+
+### Back-end
+
+`npm run backend`
+
+
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./views/Todo/Todo";
+
+const App = () => {
+  return (
+    <>
+      <div className="home-page">
+        <div className="container">
+          <Todo></Todo>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
