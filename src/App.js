8076f70efc2f6ccd03f53bccccd3db5aa6732537
Hower454516977 <454516977@qq.com>
2022-11-27 16:17:46 +0800

    update frame and fix style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import Todoist from "./components/Todoist";
+import { useState } from 'react'
+import { nanoid } from 'nanoid';
+import './index.css'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
-  return (
-    <div className="container">
-      <Todoist />
+  const [showAdd, setShowAdd] = useState(false)
+  const [tasks, setTasks] = useState([])
+  const [fold, setFold] = useState(true)
+ 
+  const loadAll = () => {
+    let allTasks = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (key.charAt(0) === '#') {
+        let oneTask = localStorage.getItem(key);
+        allTasks = [...allTasks, JSON.parse(oneTask)]
+      }
+    }
+    setTasks(allTasks)
+  }
+
+  const addTask = (task) => {
+    const id = nanoid()
+    localStorage.setItem('#' + id, JSON.stringify({
+      id: id,
+      text: task.text,
+      note: task.note,
+      important: false
+    }))
+    if (task.fold) {
+      setFold(!fold)
+      setShowAdd(!showAdd)
+    }
+    loadAll()
+  }
+
+  const deleteTask = (task) => {
+    localStorage.removeItem('#' + task.id);
+    loadAll()
+  }
+
+  const toggleTask = (task) => {
+    localStorage.setItem('#' + task.id, JSON.stringify({
+      id: task.id,
+      text: task.text,
+      note: task.note,
+      important: !task.important
+    }))
+    loadAll()
+  }
+
+  window.onload = function () {
+    loadAll()
+  }
+  
+  return (<div className='container'>
+    <div className="todoist innerbox" >
+      <Header
+        title="Todoist"
+        onClick={() => { setShowAdd(!showAdd); setFold(!fold) }}
+        cardName={fold ? 'Arrange' : 'Fold'}
+      />
+      {showAdd && !fold && <AddTask addTask={addTask} />}
+      {tasks.length > 0 ?
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleTask} />) :
+        (!showAdd && <div style={{
+          display: 'flex', height: '75%', fontSize: '50px', opacity: '0.05',
+          justifyContent: 'center', alignItems: 'center', userSelect: 'none'
+        }}>    
+            No task
+        </div>
+        )}
+    </div>
     </div>
   );
 }
+
 export default App;
